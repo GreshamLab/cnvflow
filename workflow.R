a46aa80228a43aa45acf0982d404e0e526c6b739
Titir De <td2101@nyu.edu>
2021-09-22 15:52:03 -0400

    Step 3 change

--- workflow.R
+++ workflow.R
@@ -65,7 +65,7 @@ file.rename(dir(pattern = "Experiment-Markers.csv"),"EE_GAP1_ArchMuts_2021-Exper
 #Results in a gating file, and gates applied to all samples in the gating set.
 #Author: Titir
 
-#First transform the data
+#First we transform the data
 timept01_transformed <- cyto_transformer_logicle(timept01_gating_set,
                                               channels = c("FSC-A", "FSC-H", "SSC-A", "SSC-H", "B2-A"))
 
