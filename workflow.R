bdfe69933ee753162796a53d7f8d879d3cc26dad
Julie Chuong <jc10007@nyu.edu>
2022-01-06 12:29:29 -0800

    normalize then median

--- workflow.R
+++ workflow.R
@@ -180,7 +180,7 @@ stats_median_gatewise_01 <- cyto_stats_compute(transformed_timepoint_gating_set0
 
 timepoint_raw_list <- cyto_extract(transformed_timepoint_gating_set, parent = "Single_cells", raw = TRUE, channels = c("FSC-A", "B2-A")) #raw flow data of each single cell as a list of matrices
 
-sc_distributions <- map_df(timepoint_raw_list, ~as.data.frame(.x), .id="name") %>% #convert to df, put list name in new column
+map_df(timepoint_raw_list, ~as.data.frame(.x), .id="name") %>% #convert to df, put list name in new column
   mutate(name = as.factor(name)) %>% #convert `name` to factor
   left_join(experiment_details %>% #join by name column to add metadata
   mutate(generation = as.factor(unique(experiment_details$generation)))) %>%
@@ -193,7 +193,7 @@ sc_distributions <- map_df(timepoint_raw_list, ~as.data.frame(.x), .id="name") %
 # instead of looking at the MEDIAN GFP values per population per generation,
 # we want to see the ENTIRE DISTRIBUTION of GFP of the cells per population per generation.
 
-sc_distributions %>%
+sc_distributions_g8 <- read.csv("01_02_04_v2_SingleCellDistributions_01_EE_GAP1_ArchMuts_2021.csv", stringsAsFactors = T) %>% mutate(generation = factor(generation, levels = unique(sc_distributions_g8$generation))) %>%
   mutate(name = factor(name, levels = unique(rev(c("Experiment_042-Plate_001-Reference Group-B3 Unstained (Cells).fcs",
                             "Experiment_042-Plate_001-1 copy control-D3 DGY500.fcs",
                             "Experiment_042-Plate_001-Reference Group-F3 DGY1315 mCitrine (Cells).fcs",
@@ -265,81 +265,7 @@ ggplot(aes(x = B2A_FSC, y = Description, fill = Description)) +
         axis.text.y = element_text(family="Arial", size = 10, color = "black"))
   theme_minimal()
 
-#Draw histograms of the stock strains to see if CNVs appeared at gen8 or not
-stock_folder_path = "/Volumes/GoogleDrive/My Drive/greshamlab/Molecular Determinants of CNV Evolution Dynamics/Summer 2021 Group LTEE/mCitrine CNV reporter test"
-all_files = list.files(paste0(stock_folder_path,"/FCS_files"))
-#files_pos <- match(sample_sheet$name, all_files) #find positions of files that match the sample_sheet names
-#all_files[files_pos] #subset of files to read in
-stocks_gs <- cyto_setup(path = paste0(stock_folder_path,"/FCS_files"), restrict = T, details = F) #must specify a folder, all fcs files will be read
 
-sample_sheet = read_csv(file = paste0(stock_folder_path,"/sample_sheet.csv"))
-sample_sheet = sample_sheet %>%
-  mutate(name = paste0(name, ".fcs"))
-
-#annotate experiment details using sample sheet
-for(i in 1:length(names(sample_sheet))){
-  flowWorkspace::pData(stocks_gs)[names(sample_sheet[i])]<-sample_sheet[i]
-}
-cyto_details(stocks_gs) %>% View()
-
-#transform stock strain gating set
-GFP_trans <- cyto_transformer_logicle(stocks_gs,
-                                      channels = c("B2-A", "B3-A"),
-                                      widthBasis = -10
-)#returns it as a list
-FSC_SSC_trans <- cyto_transformer_log(stocks_gs,
-                                      channels = c("FSC-A", "FSC-H", "SSC-A", "SSC-H")
-)
-combined_trans <- cyto_transformer_combine(GFP_trans,FSC_SSC_trans)
-transformed_timepoint_gating_set <- cyto_transform(stocks_gs,
-                                                   trans = combined_trans)
-
-#apply gating template to stock strain gating set
-cyto_gatingTemplate_apply(transformed_timepoint_gating_set, gatingTemplate= "cytek_gating_01_02_04_v2.csv")
-
-#get raw single cell flow data of stock strains
-timepoint_raw_list <- cyto_extract(transformed_timepoint_gating_set, parent = "Single_cells", raw = T, channels = c("FSC-A", "B2-A", "B3-A")) #raw flow data of each single cell as a list of matrices
-sc_distributions <- map_df(timepoint_raw_list, ~as.data.frame(.x), .id="name") %>% #convert to df, put list name in new column
-  mutate(name = as.factor(name), #convert `name` to factor
-        B2A_FSC = `B2-A`/`FSC-A`,
-        B3A_FSC = `B3-A`/`FSC-A`) %>% #compute normalized GFP over forward scatter
-  left_join(sample_sheet) #join by name column to add other metadata to raw data
-
-
-##Plot ridgeplots of stock strains
-#B2-A channel ridgeplot of stock strains
-sc_distributions %>%
-  mutate(Strain = fct_reorder(Strain, desc(B2A_FSC))) %>% #reorder samples by y values low to high
-ggplot(aes(x = B2A_FSC, y = Strain, fill = `mCitrine copy number`)) +
-  geom_density_ridges(scale=1.5, quantile_lines = F, quantiles = 2) +
-  xlab("B2-A mCitrine fluorescence/forward scatter") +
-  ylab("Strain") +
-  ggtitle("Stock Strains Ridgeplots") +
-  theme_minimal() +
-  scale_x_continuous(limits=c(0,3))+
-  scale_y_discrete(expand = expansion(add = c(0.2, 1.5))) +
-  scale_fill_discrete(breaks=c("zero copy",
-                               "one copy",
-                               "two copy"
-                               )) + #change order of legend items
-  theme(axis.text.x = element_text(family="Arial", size = 10, color = "black"),
-        axis.text.y = element_text(family="Arial", size = 10, color = "black"))
-ggsave("stock_strains_B2A_ridgeplot_noMedians.png")
-
-#B3-A channel ridgeplot of stock strains
-sc_distributions %>%
-  mutate(Strain = fct_reorder(Strain, desc(B3A_FSC))) %>% #reorder samples by y values low to high
-  ggplot(aes(x = B3A_FSC, y = Strain, fill = `mCitrine copy number`)) +
-  geom_density_ridges(scale=1.5, quantile_lines = F, quantiles = 2) +
-  xlab("B3-A mCitrine fluorescence/forward scatter") +
-  ylab("Strain") +
-  ggtitle("Stock Strains Ridgeplots") +
-  theme_minimal() +
-  scale_y_discrete(expand = expansion(add = c(0.2, 1.5))) +
-  scale_fill_discrete(breaks=c("zero copy","one copy","two copy")) + #change order of legend items
-  theme(axis.text.x = element_text(family="Arial", size = 10, color = "black"), #edit x-tick labels
-        axis.text.y = element_text(family="Arial", size = 10, color = "black"))
-ggsave("stock_strains_ridgeplot_B3A_noMedians.png")
 
 
 #STEP 5:  Use function to perform analysis
@@ -524,7 +450,7 @@ freq = read_csv("01_02_04_v2_stats_freq_all_timepoints.csv") %>% rename(Gate = P
 medians = read_csv("01_02_04_v2_stats_median_overall_all_timepoints.csv")
 medians_bygate = read_csv("01_02_04_v2_stats_median_gatewise_all_timepoints.csv")
 cell_numbers = read_csv("01_02_04_v2_stats_cell_number_all_timepoints.csv")
-sc_distr_alltimepoints <- read.csv("01_02_04_v2_SingleCellDistributions_all_timepoints.csv", stringsAsFactors = T)
+sc_distr_alltimepoints <- read.csv("01_02_04_v2_SingleCellDistributions_all_timepoints.csv", stringsAsFactors = T) %>% mutate(generation = factor(generation, levels = unique(sc_distr_alltimepoints$generation)))
 
 # add cell number column to freq table
 freq = left_join(freq, cell_numbers) %>%
