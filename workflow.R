29296a4681bb1bd6a6c731ad0e3a8972e1b17db7
Julie Chuong <jc10007@nyu.edu>
2022-06-22 07:46:15 -0400

    edits to sliding window function

--- workflow.R
+++ workflow.R
@@ -323,6 +323,7 @@ fw_freq_and_counts %>%
   theme(text = element_text(size=12))
 
 # plot proportion of population in each gate over time for all experimental
+#original code
 plot_list = list()
 i=1
 for(exp in unique(freq$Description)) {
@@ -346,7 +347,7 @@ plot_list$`GAP1 ARS KO`
 plot_list$`GAP1 LTR KO`
 plot_list$`GAP1 LTR + ARS KO`
 
-#dev
+#dev by Julie
 plot_list = list()
 i=1
 for(exp in unique(fw_freq_and_counts$Description)) {
@@ -780,7 +781,7 @@ CNV_false_pos_df = #freq %>%
   filter(Gate %in% c("two_or_more_copy")) %>%
   select(Type, Strain, Description, generation, Gate, Frequency, Count)
 
-# draw a histogram, see distribution is normal by eye
+# draw a histogram, see if distribution is normal by eye
 hist(CNV_false_pos_df$Frequency) #looks normal but could be left skewed
   abline(v = median(CNV_false_pos_df$Frequency),col = "red",lwd = 1.5)
   abline(v = mean(CNV_false_pos_df$Frequency), col = "blue", lwd = 1.5)
@@ -788,11 +789,11 @@ hist(CNV_false_pos_df$Frequency) #looks normal but could be left skewed
 #Test for normality
 shapiro.test(CNV_false_pos_df$Frequency) #null hypothesis is that the distribution is normal. if p <0.05, then it rejects the null hypothesis and so the distribution is NOT normal.
     #W = 0.95715, p-value = 0.4886
-# Our distribution is normal. Use the mean + 1SD as the threshold value.
+# Null hypothesis cannot be rejected. Our distribution is normal. Use the mean + 1SD as the threshold value.
 # mean = 4.29, sd = 2.32
 thres_mean = mean(CNV_false_pos_df$Frequency) + sd(CNV_false_pos_df$Frequency) #6.615341
 
-#Determine Tup for each population, the generation when CNVs first appear.
+#Determine Tup for each population, the generation when CNVs first appear above the threshold frequency.
 Tup_per_pop = #freq %>%
   fw_freq_and_counts %>%
   filter(Count>70000) %>%
@@ -802,8 +803,8 @@ Tup_per_pop = #freq %>%
   group_by(sample) %>%
   slice(which.min(generation))
 
-Tup_per_pop %>% write_csv(file = "01_02_04_v2_fw_Tup_per_pop.csv")
-Tup_per_pop = read_csv(file = "01_02_04_v2_fw_Tup_per_pop.csv")
+#Tup_per_pop %>% write_csv(file = "01_02_04_v2_fw_Tup_per_pop.csv")
+#Tup_per_pop = read_csv(file = "01_02_04_v2_fw_Tup_per_pop.csv")
 
 ggplot(Tup_per_pop, aes(reorder(Description, -generation),generation, fill = Description)) +
   geom_boxplot(outlier.shape = NA) +
@@ -824,13 +825,7 @@ ggplot(Tup_per_pop, aes(reorder(Description, -generation),generation, fill = Des
   rep("black", 8) #LTR,8, #blue
 ))
 #ggsave("01_02_04_v2_fw_Tup_boxplot_042022.png")
-ggsave("01_02_04_v2_fw_Tup_boxplot_051022.png")
-theme(
-
-  axis.text.x = element_text(family="Arial", size = 10, color = "black"), #edit x-tick labels
-  axis.text.y = element_text(family="Arial", size = 10, color = "black"),
-
-  strip.text = element_text(size=12)
+#ggsave("01_02_04_v2_fw_Tup_boxplot_051022.png")
 
 # ANOVA to test for significance
 # One Way ANOVA because there is only 1 independent variable, genotype.
@@ -843,7 +838,8 @@ summary(Tup_anova)
 # Conclusion: There IS a significant difference in the means. Genotype has has significant effect on Tup.
 
 #Calculate Sup
-#S1 Text. Calculation of CNV dynamics parameters.
+# "Sup is the rate of increase in CNV abundance during the initial expansion of the CNV subpopulation" Lauer et al. 2018
+# S1 Text. Calculation of CNV dynamics parameters.
 # Graphic representation of linear fit (and corresponding R2 values)
 # during initial population expansion of CNV alleles.
 # Slope of the linear fit corresponds to the dynamics parameter Sup shown in
@@ -852,8 +848,8 @@ summary(Tup_anova)
 # accessed in OSF: https://osf.io/fxhze/. CNV, copy number variant.
 
 equation = function(x) {
-  lm_coef <- list(a = round(coef(x)[1], digits = 2),
-                  b = unname(round(coef(fit)[2], digits = 2)),# get rid of it printing c()
+  lm_coef <- list(a = round(coef(x)[1], digigts = 2),
+                  b = unname(round(coef(fit)[2], digits = 4)),# get rid of it printing c()
                   r2 = round(summary(x)$r.squared, digits = 2));
   lm_eq <- substitute(slope == b~~~~italic(R)^2~"="~r2,lm_coef)
   as.character(as.expression(lm_eq));
@@ -874,12 +870,16 @@ ln_table = fw_freq_and_counts %>%
 # as part of the function, include a variable that changes number of fit points (window width)
 pop_list = unique(ln_table$sample)
 gens = unique(ln_table$generation)
-#function(num_fitpoints, population){
-sliding_fit = function(num_fitpoints, population){ #function to apply for loop to each of 28 populations
+
+#function to apply for loop to each of 28 populations, used some code from Lauer et al. 2018
+#dev
+num_fitpoints = 4
+population = pop_list[26]
+sliding_fit = function(num_fitpoints, population){
   timepoints = nrow(subset(ln_table, sample %in% c(population)))
   #timepoints = subset(ln_table, sample %in% c(population)) %>% distinct(generation) %>% nrow()
   rounds = timepoints- (timepoints/num_fitpoints) - 1
-  m <- matrix(ncol = 5, nrow = rounds) # nrow = number of iterations . number of iterations depend on the number of generations and the number and fitpoints. max num of generations = 24. minimum num of fitpoints is 2. therefore nrow max is 23.
+  m <- matrix(ncol = 5, nrow = rounds) #nrow = number of iterations. number of iterations depend on the number of generations and the number of fitpoints. max num of generations = 24. minimum num of fitpoints is 2. therefore nrow max is 23.
   colnames(m) <- c("start", "end", "gen_start", "gen_end", "rsquared")
   start = 1
   end = num_fitpoints
@@ -903,7 +903,7 @@ sliding_fit = function(num_fitpoints, population){ #function to apply for loop t
       guides(colour = guide_legend(override.aes = list(size=2))) +
       theme(legend.position = c(.15,.95), plot.title = element_text(size=14, hjust = 0.5), legend.title = element_blank(), axis.title.y = element_text(face="bold", size=12), axis.text.y = element_text(size=12), axis.title.x = element_text(face="bold", size=12), axis.text.x = element_text(size=12))
 
-      ggsave(paste0(population,"_Sup_g",gens[start],"-",gens[end],"_",num_fitpoints,"pts.png"))
+      ggsave(paste0(population,"_Sup_g",gens[start],"-",gens[end],"_",num_fitpoints,"pts.png"), width = 8, height = 5)
 
       print(paste0("From timepoints ",start," to ", end, ", generations ", gens[start], " to ", gens[end], ", rsquared was ", as.numeric(summary(fit)[8]) %>% round(2) ))  #populate a data frame? five columns: start, end, gen_start, gen_end,  Rsq.
 
@@ -921,14 +921,15 @@ sliding_fit = function(num_fitpoints, population){ #function to apply for loop t
     return(m)
 }
 
-#call the function and assign matrix to a variable
-result <- sliding_fit(4, pop_list[25])
-assign(paste0(pop_list[25],"_fits","_",10,"pts"), result) #rename and save to environment
-result <- sliding_fit(5, pop_list[21])
+#call the function and assign outputted matrix to a variable
+result <- sliding_fit(4, pop_list[27])
+assign(paste0(pop_list[27],"_fits","_",4,"pts"), result) #rename and save to environment
+result <- sliding_fit(5, pop_list[26])
 
 sliding_fit(4, population = pop_list[1])
 lapply(pop_list, sliding_fit(4, pop_list))
 
+#call the function for all pops in the pop_list using map()
 map(.x = pop_list[21], ~sliding_fit(4, .x))
 map(pop_list[1:3], sliding_fit(num_fitpoints =4))
 sliding_fit(4, pop)
